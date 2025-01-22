a6b4991f794f6ee31f611a12068e278c20ca1bc6
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2025-01-22 19:28:21 +0600

    route for check valid user

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -104,3 +104,12 @@ export const updateProfile = async (req, res) => {
         return res.status(500).json({message: "Internal server error"});
     }
 };
+
+export const checkAuth = (req, res) => {
+    try {
+        return res.status(200).json(req.user);
+    } catch (err) {
+        console.log("Error in checkAuth: ", err.message);
+        return res.status(500).json({message: "Internal server error"});
+    }
+};
--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
 import { verifyToken } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
@@ -8,5 +8,7 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 router.put("/update-profile", verifyToken ,updateProfile);
+
+router.get("/check", verifyToken, checkAuth);      // check every refresh that user is valid or not    
 
 export default router;
