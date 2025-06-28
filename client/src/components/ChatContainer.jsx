d39878fc84858a5f09dcccdaaf516a3db0235444
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2025-06-29 01:36:18 +0600

    backend data fetch little change for get chat messages and ui little change based on it

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -6,7 +6,6 @@ import MessageInput from "./MessageInput";
 
 const ChatContainer = () => {
   const { chatMessages, isChatLoading, selectedUser } = useSelector((state) => state.chat);
-  console.log(selectedUser)
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchChatMessages(selectedUser._id));
@@ -17,15 +16,16 @@ const ChatContainer = () => {
       <div className="h-full flex flex-col overflow-auto gap-5 p-5">
         {
           isChatLoading ? <div>Loading...</div> : chatMessages.map((message) => {
+            console.log(message);
             return (
-              <div key={message._id} className={`flex flex-col ${message.senderId === selectedUser._id ? "items-start" : "items-end"}`}>
-                <div className={`flex items-center gap-2 ${message.senderId === selectedUser._id ? "flex-row-reverse" : ""}`}>
+              <div key={message._id} className={`flex flex-col ${message.senderId._id === selectedUser._id ? "items-start" : "items-end"}`}>
+                <div className={`flex items-center gap-2 ${message.senderId._id === selectedUser._id ? "flex-row-reverse" : ""}`}>
                   <div className="p-2 rounded-lg bg-primary/20 w-fit max-w-md space-y-2">
-                    {message.image && <img src={message?.image} className="size-20 sm:size-28 md:size-36 object-cover" alt="sender image" />}
+                    {message?.image && <img src={message?.image} className="size-20 sm:size-28 md:size-36 object-cover" alt="sender image" />}
                     <p className="text-sm">{message.text}</p>
                   </div>
                   <div className="items-start">
-                    <img src={message.senderId === selectedUser._id ? selectedUser?.profileImage : message.sender?.profileImage || "/avatarDemo.png"} className="size-12 rounded-full object-cover" alt="" />
+                    <img src={message.senderId._id === selectedUser._id ? selectedUser?.profileImage : message.senderId?.profileImage != "" ? message.senderId?.profileImage : "/avatarDemo.png"} className="size-12 rounded-full object-cover" alt="" />
                   </div>
                 </div>
               </div>
