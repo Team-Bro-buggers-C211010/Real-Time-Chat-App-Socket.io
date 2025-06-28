20f75288a6dff5b599c47f698b448caa7b588d6b
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2025-06-29 00:28:41 +0600

    message input design and attached image file with preview

--- client/src/components/MessageInput.jsx
+++ client/src/components/MessageInput.jsx
@@ -1,6 +1,51 @@
+import { Image, Send, X } from "lucide-react";
+import { useRef, useState } from "react";
+import toast from "react-hot-toast";
+
 const MessageInput = () => {
+  const [message, setMessage] = useState('');
+  const [attachment, setAttachment] = useState('');
+  const fileInputRef = useRef(null);
+
+  const handleImage = (e) => {
+    const file = e.target.files[0];
+    if(!file.type.includes('image')) {
+      toast.error('Please select an image file');
+      return;
+    }
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setAttachment(reader.result);
+    };
+  }
+
   return (
-    <div>MessageInput</div>
+    <div className="p-4 w-full">
+      {
+        attachment  && (
+          <div className="mb-3 flex items-center gap-2">
+            <div className="relative">
+              <img className="size-12 md:size-16 object-cover rounded-lg border border-zinc-700" src={attachment} alt="Preview Attachment" />
+              <button onClick={() => setAttachment('')} className="absolute -top-1.5 -right-1.5 size-5 bg-base-300 text-red-500 rounded-full flex items-center justify-center cursor-pointer">
+                <X className="size-4" />
+              </button>
+            </div>
+          </div>
+        )
+      }
+      <form className="flex items-center gap-2">
+        <div className="flex-1 flex gap-2 items-center">
+          <input type="text" placeholder="Type a message..." value={message} onChange={(e) => setMessage(e.target.value)} className="w-full input input-bordered rounded-lg input-sm sm:input-md" />
+          <input type="file" ref={fileInputRef} accept="image/*" onChange={handleImage} className="hidden" />
+          {/* <button type="button" className={`hidden sm:flex btn btn-circle ${attachment ? "text-emerald-500" : "text-zinc-400"}`} onClick={() => document.querySelector('input[type="file"]').click()}><Image className="size-5"/></button> */}
+          <button type="button" className={`font-bold flex text-base-content btn btn-circle ${attachment ? "text-emerald-500" : "text-zinc-400"}`} onClick={() => fileInputRef.current?.click()}><Image className="size-5"/></button>
+        </div>
+        <button type="submit" className="cursor-pointer" disabled={!message || !attachment}>
+          <Send className={`size-5 ${message || attachment ? "text-emerald-500" : "text-zinc-400 cursor-not-allowed"}`} />
+        </button>
+      </form>
+    </div>
   )
 }
 
